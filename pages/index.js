7ea61c5e004e6070cb700d95229b86b2c71ca919
OperaRock <cncastro9@gmail.com>
2020-12-20 09:42:32 -0300

    nova home

--- pages/index.js
+++ pages/index.js
@@ -3,24 +3,7 @@ import { useState } from 'react';
 function Home() {
     return (
         <div>
-            <h2>Home</h2>
-            <Contador />
-            <div>Teste</div>
-        </div>
-    )
-}
-
-function Contador() {
-    const [contador, setContador] = useState(1);
-
-    function adicionarContador() {
-        setContador(contador + 1);
-    }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
+            <h1>Home</h1>
         </div>
     )
 }
